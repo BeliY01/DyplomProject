bbc71c2d41528d828b55fdce5dc95fac2b395861
Білошицький Влад Володимирович <kn212_bvv@student.ztu.edu.ua>
2024-11-27 10:54:42 +0200

    Changes in menu buttons

--- Tetris3D/Assets/Scripts/MainMenu.cs
+++ Tetris3D/Assets/Scripts/MainMenu.cs
@@ -2,13 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class MainMenu : MonoBehaviour
 {
     public string gameScene;
-    
+    public string Options;
+    public string Achievementss;
+
     public void Play()
     {
         SceneManager.LoadScene(gameScene);
+    }
+
+    public void Option()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Achievements()
+    {
+        SceneManager.LoadScene(2);
     }
 }
